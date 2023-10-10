0401087d266c607d39664ca793e68f127ca80849
lololkuku <85511468+lololkuku@users.noreply.github.com>
2023-10-11 01:53:23 +0300

    Update kannu liidu.js

--- kannu liidu.js
+++ kannu liidu.js
@@ -1139,7 +1139,7 @@
         },
         {
             btnText: "ooh ahaha cucka",
-            msg: ":jukka  ",
+            msg: "/cuck  ",
             sound: new Audio("https://i.ylilauta.org/bb/f7/bbf70790a0abccbb.m4a"),
         },
         {
